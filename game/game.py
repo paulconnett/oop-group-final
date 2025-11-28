4cac9d435d23a29bce9ac42a0807b0b948726af4
Parker Stacy <parkerstacy.19@gmail.com>
2025-11-28 14:39:49 -0700

    added shop logic, dungeon in progress

--- game/game.py
+++ game/game.py
@@ -6,39 +6,46 @@ __author__ = 'Parker Stacy'
 
 from typing import List
 from room import Room, Shop, Dungeon
+from player import Player
 
 
 class Game():
     '''
     game driver class
     '''
+    NUM_LEVELS = 10
+
     def __init__(self) -> None:
         '''
         constructor
         '''
-        self._num_levels: int = 10
         self._rooms: List[Room] = []
+        self._player: Player = Player("Player_Name")
 
-        for i in range(self._num_levels):
-            shop = Shop(i+1)
+        for i in range(Game.NUM_LEVELS):
+            shop = Shop(i+1, self._player)
             self._rooms.append(shop)
-            dun = Dungeon(i+1)
+            dun = Dungeon(i+1, self._player)
             self._rooms.append(dun)
-            print(f"Level {i+1} created!")
-
-        # Need player, etc from other modules
 
     def play_game(self) -> None:
         '''
         runs game logic
         '''
-        print("Start game!")
+        input("Welcome to the Dungeon >:)   (Press Enter to continute...)")
         rooms: List[Room] = self._rooms
 
         for curr_room in rooms:
-            curr_room.play_room()
+            curr_room.play_room(self.player)
 
-            print("Game Over!")
+        print("Game Over!")
+
+    @property
+    def player(self) -> Player:
+        '''
+        returns the current player object
+        '''
+        return self._player
 
     @staticmethod
     def main() -> None:
--- game/room.py
+++ game/room.py
@@ -9,25 +9,27 @@ from abc import ABC, abstractmethod
 from typing import List
 from item import Item
 from enemy import Enemy
+from player import Player
 
 
 class Room(ABC):
     '''
     Room class
     '''
-    def __init__(self, level: int):
+    def __init__(self, level: int, player: Player):
         self._level = level
+        self._player = player
 
     @abstractmethod
-    def play_room(self):
+    def play_room(self, player: Player):
         '''
         runs room logic (visit shop/fight monster)
         '''
 
     @abstractmethod
-    def print(self):
+    def __str__(self) -> str:
         '''
-        display upon entering the room
+        overload print method
         '''
 
     @property
@@ -42,35 +44,55 @@ class Shop(Room):
     '''
     shop class
     '''
-    temp_shop: List[Item] = []
+    TEMP_SHOP: List[Item] = []
 
-    temp_shop.append(Item("Sword", 5))
-    temp_shop.append(Item("Axe", 10))
-    temp_shop.append(Item("Potion", 3))
+    TEMP_SHOP.append(Item("Sword", 5))
+    TEMP_SHOP.append(Item("Axe", 10))
+    TEMP_SHOP.append(Item("Potion", 3))
 
-    def __init__(self, level: int):
+    def __init__(self, level: int, player: Player):
         '''
         constructor
         '''
-        super().__init__(level)
-        # self._num_items: int = 5  # Can be adjusted later
-        # self._items: List[Item] = []
-        # self._num_items: int = len(Shop.temp_shop)
-        self._items: List[Item] = Shop.temp_shop
+        super().__init__(level, player)
+        self._items: List[Item] = Shop.TEMP_SHOP
 
-    def play_room(self):
+    def play_room(self) -> None:
         '''
         player enters shop
         '''
-        self.print()
+        input(f"Welcome to Bork's Level {self.level} shop! (Press Enter to continue...)")
+        print(self)
 
-    def print(self):
+        print(f"You have {self._player.money} c")  # FIXME need a getter method here
+        user_in = input("Bork: Can I interest you in my wares? (y/n)")
+
+        if user_in == 'n':
+            input("Bork: See you next time... I hope...    (Press Enter)")
+            return
+        elif user_in == 'y':
+            self.buy_items(self._items)
+
+    def buy_items(self, items: List[Item]) -> None:
+        item = input("What's catching your eye? (Enter item number 1,2,...)")
+        item = int(item)
+
+        choice = items[item + 1]
+        if self._player.spend_money(choice.value):
+            input(f"You bought a {choice.name}!\nBork: See ya sucker!   (Press enter...)")
+
+    def __str__(self) -> str:
         '''
         prints shop inventory in readable format
         '''
-        print(f"Level {self.level} Shop")
+        rtn = ''
+        i = 1
+
         for item in self._items:
-            print(f"=== {item.name}: {item.value} ===")
+            rtn += f"=== {i} - {item.name}: {item.value} ===\n"
+            i += 1
+
+        return rtn
 
 
 class Dungeon(Room):
@@ -78,26 +100,40 @@ class Dungeon(Room):
     dungeon class
     '''
     temp_e: List[Enemy] = []
-    c_temp_e: Enemy = None
+    c_temp_e: Enemy
 
-    temp_e.append(Enemy("Goblin", 10, 5, 0, 5, 0))
-    temp_e.append(Enemy("Ogre", 50, 15, 10, 100, 3))
-    temp_e.append(Enemy("Dragon", 100, 25, 10, 250, 5))
+    temp_e.append(Enemy("Goblin", 10, 5, 0, 5))
+    temp_e.append(Enemy("Ogre", 50, 15, 10, 100))
+    temp_e.append(Enemy("Dragon", 100, 25, 10, 250))
 
-    def __init__(self, level: int):
+    def __init__(self, level: int, player: Player):
         '''
         constructor
         '''
-        super().__init__(level)
+        super().__init__(level, player)
         self._enemies: List[Enemy] = Dungeon.temp_e
 
-    def play_room(self):
+    def play_room(self, player: Player):
         enemy_num: int = random.randint(0, len(self._enemies)-1)
-        Dungeon.c_temp_e = self._enemies[enemy_num]
-        self.print()
+        Dungeon.c_temp_e = self._enemies[enemy_num]  # FIXME get monsters from monster.py file
 
-        # This is where combat will happen
-
-    def print(self):
-        print(f"Level {self.level} Dungeon")
         print(f"A {Dungeon.c_temp_e.name} has appeared! Time to throw hands!")
+
+    def combat(self, player: Player, enemy: Enemy):
+        options = ["Attack", "Use potion"]
+        for i in range(len(options)-1):
+            print(f"{i+1} - {options[i]}")
+
+        action = input(f"What will the brave {self._player.name} do?")
+        if action == '1':
+            dmg = self._player.attack_entity(Dungeon.c_temp_e)
+            print(f"{self._player.name} attacks for {dmg} damage!")
+        if action == '2':
+            pass
+            # self._player.use_potion(potion=)
+
+            # FIXME need access to player inventory, potion system is weird, but my branch
+            # might not be up to date. Stay tuned...
+
+    def __str__(self) -> str:
+        return ''
